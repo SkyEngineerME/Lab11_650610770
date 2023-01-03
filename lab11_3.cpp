0292fd30e3e72d06df7cc25800bdb4c465399a1e
Sky <guymechatronic@gmail.com>
2023-01-03 15:31:39 +0700

    lab11_3Done

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,35 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    int count = 0;
+    float sum = 0,Stdv,mean,sum1=0,sum2=0;
+    ifstream source("score.txt");
+    string textline;
+    while(getline(source, textline)){
+        sum = atof(textline.c_str());
+        sum2 += atof(textline.c_str());
+        sum1 += pow(sum,2);
+        count++;
+    }
+
+
+    mean = sum/count;
+    Stdv = sqrt(sum1/count-pow(mean,2));
+    cout << "Number of data = " << count << endl;
+    cout << setprecision(3);
+    cout << "Mean = "<< mean << endl;
+    cout << "Standard deviation = " << Stdv;
+     source.close();
+     return 0;
+}
+
+
+
+
+
